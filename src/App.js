bb63b2cf96ed6f5467ae1f3644ced31b975f3758
Hagar Sultan <97818325+Hagarsultan88@users.noreply.github.com>
2024-05-01 08:52:47 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TabTest from './Components/tab-test';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TabTest/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/tab-test.jsx
@@ -0,0 +1,31 @@
+import Tabs from "./tabs";
+import "./tabs.css";
+
+function RandomComponent() {
+    return <h1>Some random content</h1>;
+}
+
+export default function TabTest() {
+    const tabs = [
+        {
+            label: "Tab1",
+            content: <div>This is content for Tab 1</div>,
+        } ,
+
+        {
+            label: "Tab2",
+            content: <div>This is content for Tab 2</div>,
+        } ,
+
+        {
+            label: "Tab3",
+            content: <div>This is content for Tab 3</div>,
+        } ,
+    ];
+
+    function handleChange(currentTabIndex) {
+        console.log(currentTabIndex);
+    }
+
+    return <Tabs tabsContent={tabs} onChange={handleChange} />;
+}
